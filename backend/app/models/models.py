ea45d69ac5eb09ab925bd52faffb34eb665b5137
Pravalika <pravalikareddy315@gmail.com>
2026-06-14 13:55:15 +0530

    models

---
+++ backend/app/models/models.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Float
+from sqlalchemy import ForeignKey
+
+from app.database.database import Base
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String)
+    email = Column(String)
+
+    role = Column(String)
+
+    hourly_cost = Column(Float)
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String)
+
+    budget = Column(Float)
+
+    priority = Column(String)
+
+
+class Meeting(Base):
+    __tablename__ = "meetings"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    title = Column(String)
+
+    duration = Column(Float)
+
+    project_id = Column(Integer, ForeignKey("projects.id"))
+
+    cost = Column(Float)
+
+    confidence = Column(Float)
