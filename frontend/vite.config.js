2a3a33cc832d48923f50374e7c57070d82aabb97
Pravalika <pravalikareddy315@gmail.com>
2026-06-16 22:20:59 +0530

    fix vite config for production build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react],
+  plugins: [react()],
   server: {
     port: 5173
+  },
+  build: {
+    target: "es2020"
+  },
+  optimizeDeps: {
+    force: true
   }
 })
