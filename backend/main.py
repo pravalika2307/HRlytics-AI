7a5d6670c17eff7da6bd5dc8b32f62b85b8c87b5
Pravalika <pravalikareddy315@gmail.com>
2026-06-17 18:28:24 +0530

    fix backend

--- backend/main.py
+++ backend/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     "http://localhost:5173",
     "http://localhost:5174"
 ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
